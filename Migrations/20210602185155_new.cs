e9124996251df6b7217da6914283f89b39126bd5
Olesya-B <63393299+Olesya-B@users.noreply.github.com>
2021-06-03 01:04:08 +0200

    Add files via upload

---
+++ Migrations/20210602185155_new.cs
@@ -0,0 +1,141 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace NemesysZ2.Migrations
+{
+    public partial class @new : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_BlogPosts_Authors_AuthorId",
+                table: "BlogPosts");
+
+            migrationBuilder.DropTable(
+                name: "Authors");
+
+            migrationBuilder.DropIndex(
+                name: "IX_BlogPosts_AuthorId",
+                table: "BlogPosts");
+
+            migrationBuilder.DropColumn(
+                name: "AuthorId",
+                table: "BlogPosts");
+
+            migrationBuilder.AddColumn<int>(
+                name: "CategoryId",
+                table: "BlogPosts",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "UserId",
+                table: "BlogPosts",
+                type: "nvarchar(450)",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "Categories",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categories", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BlogPosts_CategoryId",
+                table: "BlogPosts",
+                column: "CategoryId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BlogPosts_UserId",
+                table: "BlogPosts",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_BlogPosts_AspNetUsers_UserId",
+                table: "BlogPosts",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_BlogPosts_Categories_CategoryId",
+                table: "BlogPosts",
+                column: "CategoryId",
+                principalTable: "Categories",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_BlogPosts_AspNetUsers_UserId",
+                table: "BlogPosts");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_BlogPosts_Categories_CategoryId",
+                table: "BlogPosts");
+
+            migrationBuilder.DropTable(
+                name: "Categories");
+
+            migrationBuilder.DropIndex(
+                name: "IX_BlogPosts_CategoryId",
+                table: "BlogPosts");
+
+            migrationBuilder.DropIndex(
+                name: "IX_BlogPosts_UserId",
+                table: "BlogPosts");
+
+            migrationBuilder.DropColumn(
+                name: "CategoryId",
+                table: "BlogPosts");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "BlogPosts");
+
+            migrationBuilder.AddColumn<int>(
+                name: "AuthorId",
+                table: "BlogPosts",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateTable(
+                name: "Authors",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Bio = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Authors", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BlogPosts_AuthorId",
+                table: "BlogPosts",
+                column: "AuthorId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_BlogPosts_Authors_AuthorId",
+                table: "BlogPosts",
+                column: "AuthorId",
+                principalTable: "Authors",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
