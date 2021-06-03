c2d5ad08011a773f599bfac7e81943c894309497
Olesya-B <63393299+Olesya-B@users.noreply.github.com>
2021-06-04 00:06:04 +0200

    Add files via upload

---
+++ Models/Category.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NemesysZ2.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        //Collection navigation property
+        public List<BlogPost> BlogPosts { get; set; }
+    }
+}
---
+++ Models/Repositories/NemesysRepository.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using NemesysZ2.Models;
+
+//using Microsoft.Exchange.WebServices.Data;
+using NemesysZ2.Models.Interfaces;
+
+namespace NemesysZ2.Models.Repositories
+{
+    public class NemesysReporisoty : INemesysRepository
+    {
+        private readonly AppDbContext _appDbContext;
+        private readonly ILogger _logger;
+
+        public NemesysReporisoty(AppDbContext appDbContext, ILogger<NemesysReporisoty> logger)
+        {
+            try
+            {
+                _appDbContext = appDbContext;
+                _logger = logger;
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.Message);
+                throw;
+            }
+        }
+
+        public IEnumerable<BlogPost> GetAllBlogPosts()
+        {
+            try
+            {
+                //Using Eager loading with Include
+                return _appDbContext.BlogPosts.Include(b => b.Category).OrderBy(b => b.CreatedDate);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.Message);
+                throw;
+            }
+        }
+
+        public BlogPost GetBlogPostById(int blogPostId)
+        {
+            try
+            {
+                //Using Eager loading with Include
+                return _appDbContext.BlogPosts.Include(b => b.Category).Include(b => b.User).FirstOrDefault(p => p.Id == blogPostId);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.Message);
+                throw;
+            }
+        }
+
+        public void CreateBlogPost(BlogPost blogPost)
+        {
+            try
+            {
+                _appDbContext.BlogPosts.Add(blogPost);
+                _appDbContext.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.Message);
+                throw;
+            }
+        }
+
+        public void UpdateBlogPost(BlogPost blogPost)
+        {
+            try
+            {
+                var existingBlogPost = _appDbContext.BlogPosts.SingleOrDefault(bp => bp.Id == blogPost.Id);
+                if (existingBlogPost != null)
+                {
+                    existingBlogPost.Title = blogPost.Title;
+                    existingBlogPost.Content = blogPost.Content;
+                    existingBlogPost.UpdatedDate = blogPost.UpdatedDate;
+                    existingBlogPost.ImageUrl = blogPost.ImageUrl;
+                    existingBlogPost.CategoryId = blogPost.CategoryId;
+                    existingBlogPost.UserId = blogPost.UserId;
+
+                    _appDbContext.Entry(existingBlogPost).State = EntityState.Modified;
+                    _appDbContext.SaveChanges();
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.Message);
+                throw;
+            }
+        }
+
+        public IEnumerable<Category> GetAllCategories()
+        {
+            try
+            {
+                //Not loading related blog posts
+                return _appDbContext.Categories;
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.Message);
+                throw;
+            }
+        }
+
+        public Category GetCategoryById(int categoryId)
+        {
+            try
+            {
+                //Not loading related blog posts
+                return _appDbContext.Categories.FirstOrDefault(c => c.Id == categoryId);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.Message);
+                throw;
+            }
+        }
+
+
+    }
+
+
+}
