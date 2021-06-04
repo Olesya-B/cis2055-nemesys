68fddc9f6619a501779d11c03e9ff69680a561c4
Olesya-B <63393299+Olesya-B@users.noreply.github.com>
2021-06-04 05:19:29 +0200

    Add files via upload

---
+++ Models/Repositories/NemesysRepository.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using NemesysZ2.Models;
+
+//using Microsoft.Exchange.WebServices.Data;
+using NemesysZ2.Models.Interfaces;
+
+namespace NemesysZ2.Models.Repositories
+{
+    public class NemesysRepository : INemesysRepository
+    {
+        private readonly AppDbContext _appDbContext;
+
+        public NemesysRepository(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+        public IEnumerable<BlogPost> GetAllBlogPosts()
+        {
+            //Using Eager loading with Include
+            return _appDbContext.BlogPosts.Include(b => b.Category).OrderBy(b => b.CreatedDate);
+        }
+
+        public BlogPost GetBlogPostById(int blogPostId)
+        {
+            //Using Eager loading with Include
+            return _appDbContext.BlogPosts.Include(b => b.Category).Include(b => b.User).FirstOrDefault(p => p.Id == blogPostId);
+        }
+
+        public void CreateBlogPost(BlogPost blogPost)
+        {
+            _appDbContext.BlogPosts.Add(blogPost);
+            _appDbContext.SaveChanges();
+        }
+
+        public void UpdateBlogPost(BlogPost blogPost)
+        {
+            var existingBlogPost = _appDbContext.BlogPosts.SingleOrDefault(bp => bp.Id == blogPost.Id);
+            if (existingBlogPost != null)
+            {
+                existingBlogPost.Title = blogPost.Title;
+                existingBlogPost.Content = blogPost.Content;
+                existingBlogPost.UpdatedDate = blogPost.UpdatedDate;
+                existingBlogPost.ImageUrl = blogPost.ImageUrl;
+                existingBlogPost.CategoryId = blogPost.CategoryId;
+                existingBlogPost.UserId = blogPost.UserId;
+
+                _appDbContext.Entry(existingBlogPost).State = EntityState.Modified;
+                _appDbContext.SaveChanges();
+            }
+        }
+
+
+        public IEnumerable<Category> GetAllCategories()
+        {
+            //Not loading related blog posts
+            return _appDbContext.Categories;
+        }
+
+        public Category GetCategoryById(int categoryId)
+        {
+            //Not loading related blog posts
+            return _appDbContext.Categories.FirstOrDefault(c => c.Id == categoryId);
+        }
+
+
+
+
+
+    }
+
+
+}
